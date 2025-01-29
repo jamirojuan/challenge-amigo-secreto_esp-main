edcaf668c74a50b5de05f80dabc9d8e0b189ac4d
Daniel <danielorellanaencina@gmail.com>
2025-01-28 22:12:52 -0300

    agrega inicialmente nombre a la lista y se ve

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+var listaAmigos = document.getElementById("listaAmigos");
+
+// agrega html a cuaquier elemento 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+
+// agrega elementos a la lista
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(nombreAmigo);
+    let item = document.createElement("li");
+    item.appendChild(document.createTextNode(nombreAmigo));
+    listaAmigos.appendChild(item);
+    console.log(nombreAmigo);
+}
